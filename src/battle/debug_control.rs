d40c4361571004ec6c0a9fd0fdf64437b712a7cb
Maciej Nadolski <maciejnadolski98@gmail.com>
2025-06-19 20:42:49 +0200

    Add speedup controls

--- src/battle/debug_control.rs
+++ src/battle/debug_control.rs
@@ -23,4 +23,23 @@ fn debug_control(
       time.pause();
     }
   }
+
+  let key = if keys.just_pressed(KeyCode::Digit1) {
+    Some(1.0)
+  } else if keys.just_pressed(KeyCode::Digit2) {
+    Some(2.0)
+  } else if keys.just_pressed(KeyCode::Digit3) {
+    Some(3.0)
+  } else if keys.just_pressed(KeyCode::Digit4) {
+    Some(4.0)
+  } else if keys.just_pressed(KeyCode::Digit5) {
+    Some(5.0)
+  } else {
+    None
+  };
+
+  if let Some(speedup) = key {
+    let scale = 2.0f32.powf(speedup - 1.0);
+    time.set_relative_speed(scale);
+  }
 }
